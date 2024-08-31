739e6e90aaf27ef1d6f384fdfe7d30f2154ec522
CesarGomez02 <136769734+CesarGomez02@users.noreply.github.com>
2024-08-31 14:20:48 -0600

    Initial commit

---
+++ src/app/(tabs)/playlist/index.tsx
@@ -0,0 +1,12 @@
+import { defaultStyles } from "@/styles";
+import { View } from "react-native";
+
+const PlayListScreen = () => {
+    return (
+        <View style={defaultStyles.container}>
+            {/* <Text style={defaultStyles.text}>PlayList Screen</Text> */}
+        </View>
+    )
+}
+
+export default PlayListScreen;
